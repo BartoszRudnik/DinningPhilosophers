50016d8b71a145f9de7bfadbd5a8caeac604c5bc
Bartosz Rudnik <rudnik49@gmail.com>
2021-03-08 09:46:40 +0100

    refactor

--- src/main/java/org/example/GuiForm.java
+++ src/main/java/org/example/GuiForm.java
@@ -9,13 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public class GuiForm extends JFrame implements ActionListener {
 
+    final int threadNumber = 7;
+
     private JButton stopButton;
     private JButton startButton;
+    private JTextField philo7Field;
+    private JTextField philo6Field;
     private JTextField philo5Field;
     private JTextField philo3Field;
     private JTextField philo1Field;
     private JTextField philo4Field;
     private JTextField philo2Field;
+    private JTextField fork7Field;
+    private JTextField fork6Field;
     private JTextField fork5Field;
     private JTextField fork4Field;
     private JTextField fork3Field;
@@ -26,15 +32,17 @@ public class GuiForm extends JFrame implements ActionListener {
     private JProgressBar philo3Progress;
     private JProgressBar philo4Progress;
     private JProgressBar philo5Progress;
+    private JProgressBar philo6Progress;
+    private JProgressBar philo7Progress;
 
     private JPanel mainPanel;
 
-    private final JTextField[] philoFieldArray = new JTextField[5];
-    private final JTextField[] forkFieldArray = new JTextField[5];
-    private final JProgressBar[] progressBarArray = new JProgressBar[5];
+    private final JTextField[] philoFieldArray = new JTextField[threadNumber];
+    private final JTextField[] forkFieldArray = new JTextField[threadNumber];
+    private final JProgressBar[] progressBarArray = new JProgressBar[threadNumber];
 
-    private final Philosopher[] philosophers = new Philosopher[5];
-    private final Fork[] forks = new Fork[5];
+    private final Philosopher[] philosophers = new Philosopher[threadNumber];
+    private final Fork[] forks = new Fork[threadNumber];
 
     public GuiForm() {
 
@@ -57,6 +65,8 @@ public class GuiForm extends JFrame implements ActionListener {
         philo3Progress.setStringPainted(true);
         philo4Progress.setStringPainted(true);
         philo5Progress.setStringPainted(true);
+        philo6Progress.setStringPainted(true);
+        philo7Progress.setStringPainted(true);
 
         fork1Field.setBackground(Color.GREEN);
         fork1Field.setText("Free");
@@ -68,24 +78,34 @@ public class GuiForm extends JFrame implements ActionListener {
         fork4Field.setText("Free");
         fork5Field.setBackground(Color.GREEN);
         fork5Field.setText("Free");
+        fork6Field.setBackground(Color.GREEN);
+        fork6Field.setText("Free");
+        fork7Field.setBackground(Color.GREEN);
+        fork7Field.setText("Free");
 
         philoFieldArray[0] = philo1Field;
         philoFieldArray[1] = philo2Field;
         philoFieldArray[2] = philo3Field;
         philoFieldArray[3] = philo4Field;
         philoFieldArray[4] = philo5Field;
+        philoFieldArray[5] = philo6Field;
+        philoFieldArray[6] = philo7Field;
 
         forkFieldArray[0] = fork1Field;
         forkFieldArray[1] = fork2Field;
         forkFieldArray[2] = fork3Field;
         forkFieldArray[3] = fork4Field;
         forkFieldArray[4] = fork5Field;
+        forkFieldArray[5] = fork6Field;
+        forkFieldArray[6] = fork7Field;
 
         progressBarArray[0] = philo1Progress;
         progressBarArray[1] = philo2Progress;
         progressBarArray[2] = philo3Progress;
         progressBarArray[3] = philo4Progress;
         progressBarArray[4] = philo5Progress;
+        progressBarArray[5] = philo6Progress;
+        progressBarArray[6] = philo7Progress;
 
     }
 
@@ -136,16 +156,16 @@ public class GuiForm extends JFrame implements ActionListener {
 
         if (source == startButton) {
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < threadNumber; i++) {
 
                 forks[i] = new Fork(i);
 
             }
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < threadNumber; i++) {
 
                 Fork leftFork = forks[i];
-                Fork rightFork = forks[(i + 1) % 5];
+                Fork rightFork = forks[(i + 1) % threadNumber];
 
                 if (i % 2 == 0) {
                     philosophers[i] = new Philosopher(this, leftFork, rightFork, i);
